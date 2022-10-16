ad2efa7e0b4213293d1e6b39e3f021142cb331d8
Sankaran-B <107294603+Sankaran-B@users.noreply.github.com>
2022-10-16 11:00:58 +0530

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sankaran/catkinhack_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sankaran/catkinhack_ws/devel;/home/sankaran/catkindrone_ws/devel;/home/sankaran/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/sankaran/catkinhack_ws/devel;/home/sankaran/catkindrone_ws/devel;/home/sankaran/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export GAZEBO_MODEL_PATH='/home/sankaran/catkinhack_ws/install/share/mavlink_sitl_gazebo/models:'
+export GAZEBO_PLUGIN_PATH='/home/sankaran/catkinhack_ws/install/lib/mavlink_sitl_gazebo/plugins:'
+export GAZEBO_RESOURCE_PATH='/home/sankaran/catkinhack_ws/install/share/mavlink_sitl_gazebo:'
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/sankaran/catkinhack_ws/devel/lib:/home/sankaran/catkindrone_ws/devel/lib:/home/sankaran/catkin_ws/devel/lib:/opt/ros/noetic/lib:/home/sankaran/catkinhack_ws/install/lib/mavlink_sitl_gazebo/plugins'
+export PKG_CONFIG_PATH="/home/sankaran/catkinhack_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/sankaran/catkinhack_ws/build'
+export PYTHONPATH="/home/sankaran/catkinhack_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
